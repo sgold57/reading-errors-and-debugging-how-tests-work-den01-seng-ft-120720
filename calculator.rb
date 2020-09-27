ad4a28da7abcb9f0796d74dac84cf2b71572e1d7
Sam Gold <sgold5757@gmail.com>
2020-09-27 16:57:33 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
-first_number = "Hello world!"
+first_number = 4
+
+second_number = 2
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
